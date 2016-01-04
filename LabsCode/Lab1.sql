064f46c1d1f720127337a439244db74030a25d3b
jarturomora <jarturomora@gmail.com>
2016-01-04 11:04:57 -0600

    Code for Challenge 2 addes

--- LabsCode/Lab1.sql
+++ LabsCode/Lab1.sql
@@ -1,9 +1,28 @@
 --Code for Lab 1 - Introduction to T-SQL
 
---Challenge 1: Retrieve customer data
+---------------------------------------
+--Challenge 1: Retrieve Customer Data--
+---------------------------------------
 --Step 1: Retrieve customers details
-SELECT * FROM SalesLT.Customer;
+SELECT TOP 10 * FROM SalesLT.Customer;
 --Step 2: Retrieve customer name data
 SELECT Title, FirstName, ISNULL(MiddleName, 'None') AS 'Middle Name', LastName, ISNULL(Suffix, 'None') AS Suffix
 FROM SalesLT.Customer
 ORDER BY FirstName;
+--Step 3: Retrieve customer names and phone numbers
+SELECT SalesPerson, Title + ' ' + LastName AS CustomerName, Phone
+FROM SalesLT.Customer
+ORDER BY SalesPerson;
+
+--------------------------------------------------
+--Challenge 2: Retrieve Customers and Sales Data--
+--------------------------------------------------
+--Step 1: Retrieve a list of customer companies
+SELECT STR(CustomerID) + ':' + CompanyName AS 'Customer Companies'
+FROM SalesLT.Customer
+ORDER BY 'Customer Companies';
+--Step 2: Retrieve a list of sales order revisions
+SELECT * FROM SalesLT.SalesOrderHeader;
+SELECT SalesOrderNumber + ' (' + REPLACE(STR(RevisionNumber), ' ', '') + ')' AS 'Record of Sales Orders'
+FROM SalesLT.SalesOrderHeader
+ORDER BY SalesOrderNumber;
